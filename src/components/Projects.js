2b692e580c724ef2bf927f86d91459375f40008a
thummel1867 <thummel1867@gmail.com>
2022-09-12 10:12:04 -0400

    updates Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,48 @@
 import React from "react";
-import { ScrollContainer, ScrollPage, Animator, Move, Sticky, FadeIn, ZoomInScrollOut, Zoom, batch, Fade, MoveOut, ZoomIn, StickyIn } from "react-scroll-motion";
-import logo from '../Photos/feathers.png'
+import {
+  ScrollContainer,
+  ScrollPage,
+  Animator,
+  Move,
+  Sticky,
+  FadeIn,
+  ZoomInScrollOut,
+  Zoom,
+  batch,
+  Fade,
+  MoveOut,
+  ZoomIn,
+  StickyIn,
+} from "react-scroll-motion";
+import logo from "../Photos/feathers.png";
 import MovingComponent from "react-moving-text";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Projects = () => {
-
-const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn())
-const FadeUp = batch(Fade(), Sticky(), Move())
+  const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const FadeUp = batch(Fade(), Sticky(), Move());
 
   return (
     <div className="page-header">
-        <MovingComponent
-          className="headerh2"
-          type="fadeInFromTop"
-          duration="1000ms"
-          delay="0s"
-          direction="normal"
-          timing="ease-in"
-          iteration="1"
-          fillMode="none"
-        >
-          <Link className="bio-name-link" to="/">Thomas Hummel</Link>
-        </MovingComponent>
-        <br></br>
-        <MovingComponent
-          className="headerh2"
-          type="fadeInFromBottom"
-          duration="1000ms"
-          delay="0s"
-          direction="normal"
-          timing="ease-in"
-          iteration="1"
-          fillMode="none"
-        >
-          Projects
-        </MovingComponent>
+      <MovingComponent
+        className="headerh2"
+        type="fadeInFromTop"
+        duration="1000ms"
+        delay="0s"
+        direction="normal"
+        timing="ease-in"
+        iteration="1"
+        fillMode="none"
+      >
+        <Link className="projects-name-link" to="/">Thomas Hummel</Link>
+      </MovingComponent>
+
+      <br></br>
 
       <ScrollContainer className="scroll-container">
-      <MovingComponent
-          className="headerh2"
+        <MovingComponent
+          // className="bio-content"
           type="fadeInFromBottom"
           duration="1000ms"
           delay="0s"
@@ -50,20 +53,33 @@ const FadeUp = batch(Fade(), Sticky(), Move())
         >
           <ScrollPage page={0}>
             <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-                <h2 className="animate">Feathers</h2>
-                <img className="projects-photo" src= {logo} alt="feathers"/>
+              <h2 className="animate">Feathers</h2>
+              <div className="feathers-div">
+                <img className="projects-photo" src={logo} alt="feathers" />
+                <div className="projects-content">
+                  <p className="projects-p">
+                    This app allows for birdwatchers to discover information
+                    about birds, track birds they have seen and are searching
+                    for, and add new birds to the database.
+                    <br></br>
+                    <br></br>
+                    It is a full-stack Django application.
+                  </p>
+                </div>
+              </div>
             </Animator>
-        </ScrollPage>
+          </ScrollPage>
         </MovingComponent>
+
         <ScrollPage page={1}>
-            <Animator animation={ZoomInScrollOut}>
-                <h2 className="animate">I am a Martian</h2>
-            </Animator>
+          <Animator animation={ZoomInScrollOut}>
+            <h2 className="animate">I am a Martian</h2>
+          </Animator>
         </ScrollPage>
         <ScrollPage page={2}>
-            <Animator animation={FadeUp}>
-                <h2 className="animate" >Fade Up, Dude!</h2>
-            </Animator>
+          <Animator animation={FadeUp}>
+            <h2 className="animate">Fade Up, Dude!</h2>
+          </Animator>
         </ScrollPage>
       </ScrollContainer>
     </div>
